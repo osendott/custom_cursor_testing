cd147513f98d01f65d900ca353325c6eca9459ac
osendott <william.osendott@gmail.com>
2015-05-18 13:55:31 -0400

    update script, full GUI

--- custom-cursors.sh
+++ custom-cursors.sh
@@ -26,30 +26,31 @@ wait
 dialog --yes-label "light" --no-label "dark" --yesno "Please choose base:" 6 25
 choice=$?
 case $choice in
-  1) 		
+  1) 	
 (cd $PWD/src;
         find . -type f -name '*.svg' -print0 | while IFS= read -r -d '' file; do
 if [[ `grep "#e8e8e8" "$file"` ]]; then
-echo -ne "Generating change $count \r"
+dialog --title '' --infobox "Creating dark base $count" 10 50
 sed -i "s/#e8e8e8/#ff0000/g" "$file"
 fi
 count=$((count+1))
 if [[ `grep "#2d2d2d" "$file"` ]]; then
-echo -ne "Generating change $count \r"
+dialog --title '' --infobox "Creating dark base $count" 10 50
 sed -i "s/#2d2d2d/#e8e8e8/g" "$file"
 fi
 count=$((count+1))
 if [[ `grep "#ff0000" "$file"` ]]; then
-echo -ne "Generating change $count \r"
+dialog --title '' --infobox "Creating dark base $count" 10 50
 sed -i "s/#ff0000/#2d2d2d/g" "$file"
 fi
 count=$((count+1))
 if [[ `grep "#ffffff" "$file"` ]]; then
-echo -ne "Generating change $count \r"
+dialog --title '' --infobox "Creating dark base $count" 10 50
 sed -i "s/#ffffff/#000000/g" "$file"
 fi
 count=$((count+1))
 done)
+
 wait
 esac
 
@@ -101,8 +102,8 @@ find . -type f -name '*.svg' -print0 | while IFS= read -r -d '' file; do
 if [[ `grep "$oldColor" "$file"` ]]; then
 sed -i "s/$oldColor/$newColor/g" "$file"
 count=$((count+1))
-echo -ne "Replacing $oldColor in file $count \r"
 wait
+dialog --title '' --infobox "add $newColor to file $count" 10 50
 fi
 done)
 
@@ -114,7 +115,7 @@ do
 if [ -f "$fileSource" ]; then
 count=$((count+1))
 file=$(echo $fileSource | cut -d'.' -f1)
-echo -ne "Generating .png file $count of $pngCount \r"
+dialog --title '' --infobox "generating .png file $count of $pngCount" 10 50
 inkscape $fileSource --export-png=$file.png --export-dpi=90 > /dev/null
 wait
 else
@@ -124,6 +125,7 @@ fi
 done
 
 # create cursor files
+dialog --title '' --infobox 'creating cursor theme...' 10 50
 curCount=`ls -1 $PWD/src/*.cursor 2>/dev/null | wc -l`
 count=0
 for CURSOR in $PWD/src/*.cursor; do
@@ -132,20 +134,21 @@ BASENAME=${BASENAME##*/}
 BASENAME=${BASENAME%.*}
 
 count=$((count+1))
-echo -ne "Generating .xmc $count of $curCount \r"
+dialog --title '' --infobox "generating cursor $count of $curCount" 10 50
 (cd $CHANGEDIR;xcursorgen $BASENAME.cursor $OUTDIR/$BASENAME > /dev/null)
 wait
 done
 
 # install theme
-echo -ne "Installing theme... \r"
+dialog --title '' --infobox "installing theme" 10 50
 cp $PWD/theme/custom_cursors/. ~/.icons/custom-cursors/ -R
 wait
 
 # clean everything up
-echo -ne "Cleaning up... \r"
+dialog --title '' --infobox "Clearning up" 10 50
 rm -rf $PWD/src
 rm -rf $PWD/theme
 rm -rf $PWD/color.tmp
 wait
+dialog --title 'Complete' --msgbox 'Cursor files have been generated and installed. Use tweak-tool to set cursor theme to custom-cursors. Enjoy!' 10 50
 exit
